4c06fcac0de7c408085c40230a0ee4d4894a9b7a
dinies <edoardo.ghini@live.it>
2022-01-10 17:55:03 +0100

    added line in setup to install vim plugins automatically

--- set_up.sh
+++ set_up.sh
@@ -8,4 +8,5 @@ ln -s dotfiles/vimrc .vimrc
 ln -s dotfiles/tmux.conf .tmux.conf
 ln -s dotfiles/gdbinit .gdbinit
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
 
