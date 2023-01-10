d5e07d43286ad3a7f53810cb6a686af4f27e2ec4
dinies <edoardo.ghini@live.it>
2023-01-10 18:01:18 +0000

    updated setup script

--- set_up.sh
+++ set_up.sh
@@ -5,30 +5,32 @@ cd ~
 rm -rf .vim
 ln -s dotfiles/vim .vim
 ln -s dotfiles/vimrc .vimrc
+
+#GDB
 ln -s dotfiles/tmux.conf .tmux.conf
 ln -s dotfiles/gdbinit .gdbinit
 ln -s dotfiles/stl-views.gdb .stl-views.gdb
-## VIM
-#requires installing exuberant-ctags package
+# VIM
+requires installing exuberant-ctags package
 ln -s dotfiles/ctags .ctags 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
-## NEOVIM
+# NEOVIM
 echo 'export VISUAL=nvim' >> ~/.bashrc
 echo 'export EDITOR="$VISUAL"' >> ~/.bashrc
 echo 'export TERM=xterm-256color' >> ~/.bashrc
+source ~/.bashrc
+
 wget  https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.deb
 sudo apt install ./nvim-linux64.deb
 rm ./nvim-linux64.deb
-mkdir -p .conf/nvim
-ln -s dotfiles/init.lua .conf/nvim/
 
-sudo apt install clangd xsel unzip
-sudo update-alternatives --install /usr/bin/clangd clangd /usr/bin/clangd-12 100
+sudo apt install -y xsel unzip 
+mkdir -p ~/.config/
+ln -s ~/dotfiles/nvim .config/nvim
 
 ## TMUX
+ln -s ~/dotfiles/tmux.conf .tmux.conf
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-
-
 
