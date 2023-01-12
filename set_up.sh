ea4e27ba2b63ab8ef1e2a08e5a28fe39ce2f2541
edoardo-kudan <edoardo@kudan.eu>
2023-01-12 15:27:35 +0000

    fix setup script

--- set_up.sh
+++ set_up.sh
@@ -2,22 +2,23 @@
 set -e
 
 #Utilities
-sudo apt install -y vim xsel unzip ripgresp locate gdb tree tmux cmake-curses-gui
+sudo apt install -y vim xsel unzip ripgrep locate gdb tree tmux cmake-curses-gui
 
 cd ~
+
+# VIM
 rm -rf .vim
 ln -s dotfiles/vim .vim
 ln -s dotfiles/vimrc .vimrc
-
-#GDB
-ln -s dotfiles/tmux.conf .tmux.conf
-ln -s dotfiles/gdbinit .gdbinit
-ln -s dotfiles/stl-views.gdb .stl-views.gdb
-# VIM
-requires installing exuberant-ctags package
+#requires installing exuberant-ctags package
 ln -s dotfiles/ctags .ctags 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
+
+
+#GDB
+ln -s dotfiles/gdbinit .gdbinit
+ln -s dotfiles/stl-views.gdb .stl-views.gdb
 
 # NEOVIM
 echo 'export VISUAL=nvim' >> ~/.bashrc
@@ -33,6 +34,6 @@ mkdir -p ~/.config/
 ln -s ~/dotfiles/nvim .config/nvim
 
 ## TMUX
-ln -s ~/dotfiles/tmux.conf .tmux.conf
+ln -s dotfiles/tmux.conf .tmux.conf
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
