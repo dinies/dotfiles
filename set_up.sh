dffe58cd9764c1a6cc2f6b479808e40d72beafbb
edoardo-kudan <edoardo@kudan.eu>
2023-04-19 16:48:27 +0100

    allow the script to be run multiple times, cleaned

--- set_up.sh
+++ set_up.sh
@@ -2,8 +2,9 @@
 set -e
 
 #Utilities
-sudo apt install -y \
-  vim xsel unzip ripgrep \
+sudo apt update &&
+  sudo  apt install -y \
+  vim neovim xsel unzip ripgrep \
   locate gdb tree tmux \
   cmake-curses-gui wget
 
@@ -11,35 +12,29 @@ cd /home/$USERNAME
 
 # VIM
 rm -rf .vim
-ln -s dotfiles/vim .vim
-ln -s dotfiles/vimrc .vimrc
+ln -sf dotfiles/vim .vim
+ln -sf dotfiles/vimrc .vimrc
 #requires installing exuberant-ctags package
-ln -s dotfiles/ctags .ctags 
-ls -la dotfiles
-ls -la
-ls -la .vim/bundle
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+ln -sf dotfiles/ctags .ctags 
+rm -rf .vim/bundle/Vundle.vim
+git clone https://github.com/VundleVim/Vundle.vim.git .vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 
 
 #GDB
-ln -s dotfiles/gdbinit .gdbinit
-ln -s dotfiles/stl-views.gdb .stl-views.gdb
+ln -sf dotfiles/gdbinit .gdbinit
+ln -sf dotfiles/stl-views.gdb .stl-views.gdb
 
 # NEOVIM
-echo 'export VISUAL=nvim' >> ~/.bashrc
-echo 'export EDITOR="$VISUAL"' >> ~/.bashrc
-echo 'export TERM=xterm-256color' >> ~/.bashrc
-source ~/.bashrc
-
-wget  https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.deb
-sudo apt install ./nvim-linux64.deb
-rm ./nvim-linux64.deb
-
-mkdir -p ~/.config/
-ln -s ~/dotfiles/nvim .config/nvim
+mkdir -p .config/
+ln -sf dotfiles/nvim .config/nvim
 
 ## TMUX
-ln -s dotfiles/tmux.conf .tmux.conf
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+ln -sf dotfiles/tmux.conf .tmux.conf
+rm -rf .tmux/plugins/tpm
+git clone https://github.com/tmux-plugins/tpm .tmux/plugins/tpm
 
+echo 'export VISUAL=nvim' >> .bashrc
+echo 'export EDITOR="$VISUAL"' >> .bashrc
+echo 'export TERM=xterm-256color' >> .bashrc
+source .bashrc
