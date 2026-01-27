99de17a7797a23daa400b9883d83a8aecfc7ec98
dinies <edoardo.ghini@live.it>
2026-01-27 17:16:43 +0000

    delete old bk

--- old_nvim_bk/lua/plugins/modules/nightfox.lua
+++
@@ -1,7 +0,0 @@
-return {
-    "EdenEast/nightfox.nvim",
-    -- possible theme styles: nightfox duskfox dayfox downfox nordfox terafox carbonfox
-    config = function ()
-      vim.cmd('colorscheme carbonfox')
-    end
-}
--- old_nvim_bk/lua/plugins/modules/undotree.lua
+++
@@ -1,7 +0,0 @@
-return {
-    "mbbill/undotree",
-    config = function ()
-      vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
-      -- TODO add to which_key_defaults
-    end
-}
